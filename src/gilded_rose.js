86462e785d65842ac8664509abe7436a54aa1330
zackluckyf <fanning.34@gmail.com>
2016-08-19 10:25:10 -0400

    gilded-rose refactored

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,50 +13,67 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-function update_quality () {
+function update_quality (items) {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-      }
-    }
+    sortItems(items[i]);
+  }
+}
+
+function sortItems (item) {
+  if (item.name === 'Sulfuras, Hand of Ragnaros') {
+    // do nothing you're guccci
+  }
+  else if (item.name === 'Aged Brie') {
+    updateBrie(item);
+  }
+  else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+    updateBackstagePasses(item);
+  }else {
+    updateNormalItems(item);
+  }
+}
+
+function updateBrie (item) {
+  var underMaxValue = item.quality < 50;
+  if (underMaxValue) {
+    item.quality++;
+  }
+  item.sell_in--;
+}
+
+function updateBackstagePasses (item) {
+  var moreThanTenDays = item.sell_in > 10;
+  var moreThanFiveDays = item.sell_in > 5;
+  var atLeastOneDay = item.sell_in > 0;
+  var overMaxValue = item.quality > 50;
+
+  item.sell_in--;
+  if (moreThanTenDays) {
+    item.quality++;
+  }
+  else if (moreThanFiveDays) {
+    item.quality += 2;
+  }
+  else if (atLeastOneDay) {
+    item.quality += 3;
+  }else {
+    item.quality = 0;
+  }
+  if (overMaxValue) {
+    item.quality = 50;
+  }
+}
+
+function updateNormalItems (item) {
+  var Expired = item.sell_in < 0;
+  var hasValue = item.quality >= 0;
+  item.sell_in--;
+  if (hasValue && Expired) {
+    item.quality -= 2;
+  }else {
+    item.quality--;
+  }
+  if (!hasValue) {
+    item.quality = 0;
   }
 }
--- test/gilded_rose.spec.js
+++ test/gilded_rose.spec.js
@@ -1,23 +1,47 @@
 describe('Gilded Rose', function () {
-  it('should have an update_quality function', function () {
-    expect(update_quality).toBeDefined();
-  });
-  it('should not change the quality of Sulfuras', function () {
-    var val1 = items[3];
-    update_quality();
-    var val2 = items[3];
-    expect(val1).toEqual(val2);
-  });
   it('should increase quality of Aged Brie', function () {
     var val1 = items[1].quality;
-    update_quality();
+    update_quality(items);
     var val2 = items[1].quality;
     expect(val1 + 1).toEqual(val2);
   });
   it('should decrease the quality of Conjured Mana Cake', function () {
     var val1 = items[5].quality;
-    update_quality();
+    update_quality(items);
     var val2 = items[5].quality;
     expect(val1 - 1).toEqual(val2);
+  });
+  it('should not decrease the sell in of Sulfuras', function () {
+    var val1 = items[3].sell_in;
+    update_quality(items);
+    var val2 = items[3].sell_in;
+    expect(val1).toEqual(val2);
+  });
+  it('should decrease quality to zero if sell_in is 0 for Backstage passes', function () {
+    items[4].sell_in = 0;
+    update_quality(items);
+    var val1 = items[4].quality;
+    expect(val1).toEqual(0);
+  });
+  it('should increase quality by three if sell_in is <6 for Backstage passes', function () {
+    var val1 = items[4].quality;
+    items[4].sell_in = 5;
+    update_quality(items);
+    var val2 = items[4].quality;
+    expect(val1 + 3).toEqual(val2);
+  });
+  it('should cap quality increase of Aged Brie to 50', function () {
+    items[1].quality = 50;
+    var val1 = items[1].quality;
+    update_quality(items);
+    var val2 = items[1].quality;
+    expect(val1).toEqual(val2);
+  });
+  it('should not let conjured mana cake fall below 0 value', function () {
+    items[5].quality = 0;
+    var val1 = items[1].quality;
+    update_quality(items);
+    var val2 = items[1].quality;
+    expect(val1).toEqual(val2);
   });
 });
